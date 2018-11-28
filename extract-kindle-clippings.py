67f0f8aa9170bafd0edbbd77ca63ac0013c520ff
Levien van Zon <levien@zonnetjes.net>
2018-11-28 01:36:00 +0100

    First commit of Python script

---
+++ extract-kindle-clippings.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python3
+
+'''
+Written by Levien van Zon (levien at gnuritas.org), 2018
+Usage: extract-kindle-clippings.py <My Clippings.txt file> [<output directory>]
+'''
+
+import re
+import hashlib
+from dateutil.parser import parse
+import os
+from datetime import datetime, timedelta, timezone
+import getpass
+import sys
+
+if len(sys.argv) > 1:
+    infile = sys.argv[1]
+else:
+    infile = 'My Clippings.txt'
+
+if not os.path.isfile(infile):
+    username = getpass.getuser()
+    infile = '/media/' + username + '/Kindle/documents/My Clippings.txt'
+    if not os.path.isfile(infile):
+        print('Could not find "My Clippings.txt", please provide the file location as an argument\nUsage: ' + argv[0] + ' <clippings file> [<output directory>]\n')
+
+if len(sys.argv) > 2:
+    outpath = sys.argv[2]
+else:
+    outpath = 'clippings/'
+
+# TODO: make output directory if it doesn't exist, by executing mkdir -p
+# if not os.path.isdir(outpath): ...
+    
+
+def getvalidfilename(filename):
+    import unicodedata
+    clean = unicodedata.normalize('NFKD', filename)
+    return re.sub('[^\w\s()\'.?!:-]', '', clean)
+    
+
+note_sep = '=========='
+
+commentstr = '.. '  # RST (reStructuredText) comment
+
+regex_title = re.compile('^(.*)\((.*)\)$')
+regex_info = re.compile(r'^- (\S+) (.*)[\s|]+Added on\s+(.+)$')
+regex_loc = re.compile('Loc\. ([\d\-]+)')
+regex_page = re.compile('Page ([\d\-]+)')
+regex_date = re.compile('Added on\s+(.+)$')
+
+regex_hashline = re.compile('^\.\.\s*([a-fA-F0-9]+)' + '\s*')
+
+
+pub_title = {}
+pub_author = {}
+pub_notes = {}
+pub_hashes = {}
+
+notes = {}
+locations = {}
+types = {}
+dates = {}
+
+existing_hashes = {}
+
+print('Scanning output dir', outpath)
+for directory, subdirlist, filelist in os.walk(outpath):
+    for fname in filelist:
+        ext = fname[-4:]
+        if ext == '.rst' or ext == '.RST':
+            print('Found RST file', fname, 'in directory', directory)
+            # open file, find commend lines, store hashes
+            rst = open(directory + '/' + fname, 'r')
+            line = rst.readline()
+            lines = 0
+            hashes = 0
+            while line:
+                lines += 1
+                findhash_result = regex_hashline.findall(line)
+                if len(findhash_result):
+                    foundhash = findhash_result[0]
+                    existing_hashes[foundhash] = fname
+                    hashes += 1
+                line = rst.readline()
+            rst.close()
+            print(hashes, 'hashes found in', lines, 'scanned lines')
+        else:
+            print('File', fname, 'does not seem to be RST, skipping', ext)
+
+print('Found', len(existing_hashes), 'existing note hashes')
+print('Processing clippings file', infile)
+        
+mc = open(infile, 'r')
+
+mc.read(1)  # Skip first character
+
+line = mc.readline().strip()
+        
+while line:
+    
+    key = line.strip()
+    result_title = regex_title.findall(key)    # Extract title and author
+    line = mc.readline().strip()                # Read information line
+    note_type, location, date = regex_info.findall(line)[0]    # Extract note type, location and date
+    result_loc = regex_loc.findall(location)
+    result_page = regex_page.findall(location)
+    if len(result_title):
+        title, author = result_title[0]
+    else:
+        title = key
+        author = 'Unknown'
+        
+    if len(result_loc):
+        note_loc = result_loc[0]
+    else:
+        note_loc = ''
+        
+    if len(result_page):
+        note_page = result_page[0]
+    else:
+        note_page = ''
+        
+    note_text = ''
+    line = mc.readline()                # Skip empty line
+    line = mc.readline().strip()
+        
+    while line != note_sep:
+        note_text += line + '\n'
+        line = mc.readline().strip()
+    
+    note_hash = hashlib.sha256(note_text.strip().encode('utf8')).hexdigest()[:8]
+    
+    if key not in pub_notes:
+        pub_notes[key] = []
+        pub_hashes[key] = []
+        
+    pub_title[key] = title.strip()
+    pub_author[key] = author.strip()
+    pub_notes[key].append(note_text.strip())
+    pub_hashes[key].append(note_hash)
+    
+    locstr = ''
+    if note_loc:
+        locstr = 'loc.' + note_loc
+    if note_page:
+        if note_loc:
+            locstr += ', '
+        locstr += 'p.' + note_page
+        
+    try:
+        datestr = str(parse(date))
+    except:
+        datestr = date
+    
+    notes[note_hash] = note_text.strip()
+    locations[note_hash] = locstr
+    types[note_hash] = note_type
+    dates[note_hash] = datestr
+        
+    line = mc.readline().strip()
+
+mc.close()
+    
+for key in pub_title.keys():
+    nr_notes = len(pub_notes[key])
+    author = pub_author[key]
+    title = pub_title[key]
+    short_title = title.split('|')[0]
+    short_title = short_title.split(' - ')[0]
+    short_title = short_title.split('. ')[0]
+    if len(short_title) > 128:
+        short_title = short_title[:127]
+    if (nr_notes > 2):
+        fname = author + ' - ' + short_title.strip() + '.rst'
+        short = 0
+    else:
+        fname = 'short_notes.rst'
+        short = 1
+
+    new_hashes = 0
+    for note_hash in pub_hashes[key]:
+        if note_hash not in existing_hashes:
+            new_hashes += 1
+    
+    if new_hashes > 0:
+        print(new_hashes, 'new notes found for', title)
+    else:
+        continue            # Skip to next title if there are no new hashes
+    
+    outfile = outpath + getvalidfilename(fname)
+        
+    newfile = os.path.isfile(outfile)
+    
+    out = open(outfile, 'a')
+    
+    if short:
+        # Short note, output a small header and append to short note file
+        if author != 'Unknown':
+            titlestr = author + ' - ' + title
+        else:
+            titlestr = title
+        out.write(titlestr + '\n')
+        out.write(('-' * len(titlestr)) + '\n\n')
+    elif not newfile:
+        # Many notes, output with header and metadata in a separate file
+        titlestr = 'Highlights from ' + title
+        out.write(titlestr + '\n')
+        out.write(('=' * len(titlestr)) + '\n\n')
+        if author != 'Unknown':
+            out.write(':authors: ' + author + '\n\n')
+            
+    last_date = datetime.now()
+    
+    for note_hash in pub_hashes[key]:
+        note = notes[note_hash]
+        note_type = types[note_hash]
+        note_date = dates[note_hash]
+        note_loc = locations[note_hash]
+        if note_hash in existing_hashes:
+            print('Note', note_hash, 'is already in', existing_hashes[note_hash])
+        else:
+            print('Adding new note to', outfile + ':', note_hash, note_type, note_loc, note_date)
+            
+            comment = str(commentstr + note_hash + ' ; ' + note_type + ' ; ' + note_loc + ' ; ' + note_date)
+            
+            if short:
+                comment += ' ; ' + author + ' ; ' + title
+                
+            out.write(comment + '\n\n')
+            out.write(note + '\n\n')
+        try:
+            last_date = parse(note_date)
+        except:
+            pass
+            
+    out.close()
+    
+    # Update file modification time to time of last note
+    
+    if last_date.tzinfo is None or last_date.tzinfo.utcoffset(last_date) is None:
+        epoch = datetime(1970, 1, 1)
+    else:    
+        epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
+    note_timestamp = (last_date - epoch) / timedelta(seconds=1)    
+    os.utime(outfile, (note_timestamp, note_timestamp))
+    
+
